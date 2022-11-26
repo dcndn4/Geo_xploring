b5aba6e4e2465b540f9016931adb2c8259ed2887
dcndn4 <stokes@iphouse.com>
2022-11-26 08:09:06 -0600

    first!

---
+++ Worldle basically.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 08:02:55 2022
+
+@author: CS_Knit_tinK_SC
+"""
+
+# worldle workings!
+
+# since no size indication, 1st guess is randomer than optimal
+
+# guess 1 --> venezuela
+
+# response: 85%, 2813 km se
+# Venezuela: 6.4238° N, 66.5897° W
+
+# distance Venezuela to Brazil: 2801 Km -- it's Brazil!
+
+# https://www.geodatos.net/en/distances/country/venezuela
