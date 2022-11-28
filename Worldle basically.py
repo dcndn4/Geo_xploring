bfeeb13e4d36d8a19c88c151014ed48ad343aa15
dcndn4 <stokes@iphouse.com>
2022-11-28 05:35:56 -0600

    aha!

--- Worldle basically.py
+++ Worldle basically.py
@@ -9,7 +9,7 @@ Created on Sat Nov 26 08:02:55 2022
 
 # since no size indication, 1st guess is randomer than optimal
 
-# guess 1 --> venezuela
+# guess 1 --> Venezuela
 
 # response: 85%, 2813 km se
 # Venezuela: 6.4238° N, 66.5897° W
@@ -22,3 +22,40 @@ Created on Sat Nov 26 08:02:55 2022
 Recognizable! 
 
 #%%
+
+# guess 1 --> Syria
+
+# response: 87%, 2473 km NW
+
+# Syria: 34.8021° N, 38.9968° E
+
+# to turkey 7xx
+# to Bulgaria  1,3xx
+# to Austria 2,4xx -- Winner!
+
+#%%
+
+# Vienna 48.2082° N, 16.3738° E
+# Austria 47.5162° N, 14.5501° E
+
+# degree latitude = 111 km
+# degree longitude varies from equator (111 km) to poles (0)
+
+#%%
+
+# longitude difference = 14 degrees approx
+# latitude difference = 24 degrees approx
+
+diff = (14*111)+(24*50)
+
+#%%
+
+# another take: Austria: 48.21 lat 16.37 long; Damascus 33.51 lat 36.29 long
+
+# 48.21-34.80 = 13.5 approx lat
+# 16.37-38.99 = 22.5 approx long
+
+difff = (13.5*111)+(22.5*55)
+
+#%%
+
